f7da151e4f4fe5129b01453a7a5b15906434693c
Kerndog73 <kerndog73@gmail.com>
2017-12-13 12:07:53 +1030

    Start writing parser
    The JSONNET is a bit slower than I was expecting. The JS version takes about 500ms to compile the Level 0 (450 bytes). The commandline tool takes about 4s to compile all of the levels with the entity files (22,395 bytes).

--- js/operations.js
+++ js/operations.js
@@ -6,8 +6,21 @@ $(document).ready(function() {
   fileOpener = $("#operations #file_opener");
   fileSaver = $("#operations #file_saver");
 
+  fileOpener.change(function(e) {
+    const files = e.target.files;
+    if (files.length === 0) {
+      return;
+    }
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      JSONNETtoEntities(e.target.result);
+    };
+    reader.readAsText(file);
+  });
+
   $("#open_op").click(function() {
-    fileOpener.click();
+    fileOpener[0].click();
   });
   $("#save_op").click(function() {
     const blob = new Blob([entitiesToJSONNET()], {type: "text/plain"});
@@ -74,4 +87,47 @@ function propToString(propType, prop) {
 
 function enumToString(enumObject, prop) {
   return "\"" + Object.keys(enumObject)[prop].toLowerCase() + "\"";
+}
+
+function JSONNETtoEntities(jsonnet) {
+  // Removing the import
+  // We're assuming that the first line of the file is
+  // local e = import "entities.jsonnet";
+  // if I created the file or this editor created the file then this assumption
+  // is OK.
+  // THIS CODE WILL BREAK IF SOMEONE MOVES THE IMPORT OR RENAMES THE VARIABLE
+  jsonnet = jsonnet.slice(jsonnet.indexOf("\n"));
+  const entityNames = JSON.stringify(Object.keys(FACTORIES));
+  jsonnet =
+`local e = {
+  ["make" + name]: function(params) [
+    name,
+    params
+  ] for name in ${entityNames}
+};` + jsonnet;
+
+  const entityList = JSON.parse(JSONNETtoJSON(jsonnet));
+  console.log(entityList);
+}
+
+function JSONNETtoJSON(jsonnet) {
+  const jsonnet_make = Module.cwrap('jsonnet_make', 'number', []);
+  const jsonnet_realloc = Module.cwrap('jsonnet_realloc', 'number', ['number', 'number', 'number']);
+  const jsonnet_evaluate_snippet = Module.cwrap('jsonnet_evaluate_snippet', 'number', ['number', 'string', 'string', 'number']);
+  const jsonnet_destroy = Module.cwrap('jsonnet_destroy', 'number', ['number']);
+  const vm = jsonnet_make();
+  const error_ptr = Module._malloc(4);
+  const output_ptr = jsonnet_evaluate_snippet(vm, "snippet", jsonnet, error_ptr);
+  const error = Module.getValue(error_ptr, 'i32*');
+  Module._free(error_ptr);
+  const result = Module.Pointer_stringify(output_ptr);
+  jsonnet_realloc(vm, output_ptr, 0);
+  jsonnet_destroy(vm);
+  if (error == 1) {
+    console.log(result);
+    alert("An error occured while converting JSONNET to JSON");
+    return "";
+  } else {
+    return result;
+  }
 }
